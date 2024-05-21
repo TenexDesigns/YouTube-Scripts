ce441a2b0d632deb5e1f568df12980d39c69ab9a
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-05-21 17:07:06 +0300

    Update Value.java

--- Advice for youtube/Value.java
+++ Advice for youtube/Value.java
@@ -17,8 +17,13 @@ provide value and peopele will return and pay by giving attention and sunscribin
 3. The Importance of the First Few Seconds.
 
 
-  title, when sombody reads it, do they have to watch that video, is sit soo intresting that they have to watch it.-- below 50 characters- shart simple and intereting
+  title, when sombody reads it, do they have to watch that video, is sit soo intresting that they have to watch it.-- below 50 characters- shart simple and intereting.
+  The title and thumb nails should be in the first 5 seconds so that peopel feel that what they clicked on is what they are getting
+what you put on the title and thumb nails, must also be in the intro ttoo, to keeeppeopel watching., deliever the conten and feeling in the first five seconds, pacing mood style 
+  Swithc the cammera evevry 4 seconds to retain visual attaentions, Provide the problem , and hten preent the solutiions
 
+
+  
   the more you study the algoerhm, the more you realize you are tuduyng the human psycollgy and how they ract to diifent videos and titles and content and hwta humans want to watch.
   not the algorthm diditn like that video, its the audience diidnt like that video,
   the algoerhm relects what the peopel want.
