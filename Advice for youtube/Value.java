6f3886df9f150b9c76b942e320db875c50c0750b
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-05-21 16:05:34 +0300

    Create Value.java

---
+++ Advice for youtube/Value.java
@@ -0,0 +1,25 @@
+provide value and peopele will return and pay by giving attention and sunscribing.
+  Just start, dont overthink it
+  .
+  We laern by doing.
+  Succes is outside of you comfort zone.
+  Thinking like a viewer instead of a youtuber  or of what the algorithm wants.
+  Not qitting is the number one recipy of success, keep trying and improving.
+  Consistency is what keeeps you in the game, dont let discouragement keep you out of doidng what you want.
+  How to make good and great videos pepel actualy want to watch to get views.
+  Improve something alaways, and when you make good content, you will blow up.
+  you have to make mistakes over time and imprive to be greate.
+  Youtube wants peopel to click on a video and watch it- how do i do this, what video ideas, video title and thumbnail
+
+
+  1. Consistency and Improvement.
+2. Understanding Your Audience (Algorithm).
+3. The Importance of the First Few Seconds.
+
+
+  title, when sombody reads it, do they have to watch that video, is sit soo intresting that they have to watch it.-- below 50 characters- shart simple and intereting
+
+  the more you study the algoerhm, the more you realize you are tuduyng the human psycollgy and how they ract to diifent videos and titles and content and hwta humans want to watch.
+  not the algorthm diditn like that video, its the audience diidnt like that video,
+  the algoerhm relects what the peopel want.
+  
