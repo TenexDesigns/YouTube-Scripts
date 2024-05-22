340c28b061d78b7572c0ca6bbf5185b3a519b3bd
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-05-22 08:13:35 +0300

    Create 1_Video Ideas.java

---
+++ Video Ideas/1_Video Ideas.java
@@ -0,0 +1,17 @@
+1- Top 3 Habits to Change Youur Life
+  2_ Life Saving Hacks
+  3- Passive Income ideas -
+  Make Money WHile you are sleeping
+  Highest Paid Carrers
+  How to sell anaything
+  9 bad money habits
+  9 industries that make peopel rich
+  Habaits that make you poor
+  Why real easted creates 90% of Millionares
+  The six laws of wealth
+  Rules of Money
+  Make Money While you sleep
+  Biggest Money Wasters
+  Habbits Damaging your brain
+  Bad Hanits to avaoid
+  WHat Schools DOesnt Teach you
