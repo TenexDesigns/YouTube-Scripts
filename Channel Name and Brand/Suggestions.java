0b6d9592706947d63ec44b42ca93c86bc71e7f1f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-05-23 18:58:50 +0300

    Update Suggestions.java

--- Channel Name and Brand/Suggestions.java
+++ Channel Name and Brand/Suggestions.java
@@ -11,4 +11,22 @@ Betteru
     earnwell
     lyfttalks
     youtalks
+    educare
+    fortuna
+    jivani
+    Ravida
+    uplift
+    BeBetter
+    Moring Motivation
+    Unlocket Potential
+    Mide Fuel
+    Awaken
+    InnerSpart
+    newEra
+    JoyTrek
+    Earn it
+    Worldwiser
+    offtheground
+    youngachiver
+    titnytutor
     
