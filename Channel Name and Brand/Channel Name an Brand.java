a25db163facf70ce9510bf8b5fb5f213361d3538
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-05-23 18:14:34 +0300

    Create Channel Name an Brand.java

---
+++ Channel Name and Brand/Channel Name an Brand.java
@@ -0,0 +1,5 @@
+Tenex , Motivation, inspiration, educational, youtube , imporve, learn, earn, succend, better, 
+
+
+
+  its a youtube channel to hep and improve peopelse lives, through motivational and uplifting videos
