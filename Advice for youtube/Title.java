0a734d521e1268fe35821b874ccffc3c93b5dbd0
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-05-25 07:57:53 +0300

    Create Title.java

---
+++ Advice for youtube/Title.java
@@ -0,0 +1,26 @@
+optimiz title for search
+  50 to 60 charateters, beacuse youtube cut characters at 64
+  the title shoudl drwa attention and clearly coomunuiactes the value of your video
+  it should contain keywods , viewwers are searching for
+
+
+
+  thumb nails
+  - have a center image, about the videos, that convayes what the video is about, and some few words 3-5 at most to tell what its about
+  - the 3 -5 words should catch views attentions , e.g My biigest mistake - dont buy this
+
+
+  tags
+  # help tell youtube what your video is all about
+  tags are like lables that push your videos to the right viewers
+  e.g #oragnes  #orangejuice #orangecake #freshoranges #bestoranages #tastyoranges   are better tags for  a video about oranges than tis tags about the same video
+  #fruits, #tastyfruits #goodfruits #bestfruits  #fruitjuice
+  add tags relevant to your video, and not hot or trending tas such as #drake #mrbeast #tailorswift
+
+
+  the discriptions
+
+  never understimat community enegament, -ask questions to receive enegamenent- this creates a loyal abideoence and also sends signals to youtube
+  youchannel  is crating a convesation whicj boosts your chananle
+  
+  
