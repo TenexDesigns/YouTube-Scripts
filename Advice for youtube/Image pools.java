9bcb0ca829f050db9c615ff0620ef0eea2bbbacf
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-05-25 08:20:59 +0300

    Create Image pools.java

---
+++ Advice for youtube/Image pools.java
@@ -0,0 +1,24 @@
+give usess 4 options
+  have one word answers
+
+
+  choose a question that will apeal to a broad audienc
+  keep everuthing short- from the question to the answers
+
+
+
+  long form commnity post
+
+
+
+  GET DISCOVERED ON YOUTUBE
+  - YOUTUBE NOW USES ARTIFICIAL INTELLIGENCE TO RECOMMEND VIDEOS
+
+
+  - YOUR TITLE SPEAKS A LOT
+  my recommendation , is start with the a word or words: the the video intended
+  e.g Small channels: Do this to grow your subceibers count
+
+
+  incude capital laters
+  
