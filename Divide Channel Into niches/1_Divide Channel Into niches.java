716a983bdd78c605a3e8830ce86998ed190321bf
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-05-30 16:47:06 +0300

    Update 1_Divide Channel Into niches.java

--- Divide Channel Into niches/1_Divide Channel Into niches.java
+++ Divide Channel Into niches/1_Divide Channel Into niches.java
@@ -1,12 +1,14 @@
-TenexDesigns - ASMR motivation Motivation and improvemnet, and asmr motivation, 
-  TenexDesigns - Pets
-  TenexDesigns - Shorts channel- and parodies -- and skits--e.g batman and suerman life advice--- Movies and Series  ---  discuss celebritis, musicans and actors, thei back stories and opinions about movies and rreviews
+TenexDesigns - ASMR motivation Motivation and improvemnet, and asmr motivation, discussmm motivation in soothing tome and engouragement, and has someasmr elemets, can have speeches of motivatios, and personal improvemt, relations , datind and anything selef improvevmt
+  TenexDesigns - Pets - talks about animals an pets in genral
+  TenexDesigns - Shorts channel- and parodies -- and skits--e.g batman and suerman life advice---,  
   TenexDesigns - Finance - discuss bussines and people in hsitory and the bsuusines they didi
-  TenexDesigns - Music
-  TenexDesigns - ShortStories  --- use pictures on pininters to tell storoes, both scary and of real life peopelw
-  
-    
-    
+  TenexDesigns - Music And Movies  - - Movies and Series  ---  dut movies and rreviews, dicsuss actors, musciand and celebrities
+  TenexDesigns - ShortStories  --- use pictures on pininters to tell storoes, both scary and of real life peopelw, iscuss celebritis, musicans and actors, thei back stories and opinions abo
+
+    ---ED1C24   - red
+  000000   - black
+   F7931E  -  oragenge
+  F15A24
     
 
     
