24c189409905a8023e28b2718acbf778c8ae9efe
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-06-08 05:57:33 +0300

    Create Master Qoogerw Quotes.java

---
+++ Master Qoogerw Quotes.java
@@ -0,0 +1,12 @@
+"Why did the coffee file a police report? It got mugged."
+
+"I’m reading a book on anti-gravity. It’s impossible to put down."
+
+"How does Moses make his coffee? Hebrews it."
+
+"Why don’t oysters donate to charity? Because they are shellfish."
+  "What’s orange and sounds like a parrot? A carrot."
+  "I used to be a baker, but I couldn’t make enough dough."
+  "Why did the golfer bring two pairs of pants? In case he got a hole in one."
+
+"Why do cows have hooves instead of feet? Because they lactose."
