caad0aa07986941eac8a308fa9ba0f512f80d776
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-07-20 08:41:16 +0300

    Create history 23.java

---
+++ Peopel/history 23.java
@@ -0,0 +1,61 @@
+What the Red Sea ship attacks are really about,
+The real reason Egypt is moving its capital,
+Why China is winning the EV war,
+  What the Red Sea ship attacks are really about,
+Why Queen Elizabeth II was the queen of 15 countries,
+  2023 in 5 minutes , how screen affect your sleep, 
+  the rise of xi ging pin expained, the rise of putin explained,
+  the rise of trunp explained, the rise of kim jong un explained,how palestines were expelled fromt ehir home, 
+  how fif corruted the world cup, Banned weapeons , putins war on ukrain expained, chinas war on taiwan expained, 
+  middel east wars explained,
+  vietman war explained, colinization explained, gang wars in usa exapled,  drug cartels in mexci expalined, 
+  elsavdor homiced explained, the taliban exapined
+
+
+  idi amin expained,
+  Rwand geniced expained
+  saddam husaain exianed,
+  Mahhatma ganthi expained
+  muammar ghadiif expained
+  jfk keendy expained
+  911 explained
+  trump assaniation exapined
+  aparthined expained
+  Martin luther explaiende
+  hitler explaiend
+  wisto churtchiil expained
+
+  joseph stalin
+  malcom x
+  messi explained
+  chritiano ronaldo expained
+
+  hensrry kissinger expained
+  why recessions happen
+  inflation expalined
+  the truth about Boeing
+  The truth About Mcdianlds
+  How china became so powerful
+  why people think the world is flar
+
+  the taliban expined
+  el cah expained
+  Pabro escobar explained
+  the ocain war in soth amerca
+  the emu war in australia
+  chian vs hon kong expained
+  Singapores rise expalined
+  why is zimababwe poor
+  from spy to pressident :The rise of putin
+  How America Became a super Powr
+  Brexit Explained
+  Isis Expained
+  Ameca Vs Hawaii Expained
+  the islae -palestine conflict expained
+  the rise of isis
+  who is alqida, 
+  Bokor haram
+  Alshabba Expalined,
+  
+
+  
